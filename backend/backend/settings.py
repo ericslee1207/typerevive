986f99f4970d0cdf84843687be1353c567b71766
Eric Lee <ericslee1207@gmail.com>
2023-06-26 12:54:36 -0700

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["herokuappname.herokuapp.com"]
 
 
 # Application definition
