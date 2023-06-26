718bcac685fb12e475d654cfe007b9656bd88375
Eric Lee <ericslee1207@gmail.com>
2023-06-26 12:55:14 -0700

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["herokuappname.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
